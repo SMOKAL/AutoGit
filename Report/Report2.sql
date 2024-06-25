94e6b2519786e6b54fd13ef1aad57f4d09c46f30
SMOKAL <Shreya.Padmakar.Mokal@gds.ey.com>
2024-06-26 04:16:49 +0530

    Update content between patterns

--- Report/Report2.sql
+++ Report/Report2.sql
@@ -14,7 +14,13 @@ Rule1Businessdata as
  (Select distinct p.id,p.name,d.profession
  from profile p join
       data d
- on p.id=d.contactid)
+ on p.id=d.contactid)This area the new changes
+
+
+
+
+
+
 #RuleEnd:Rule1Businessdata#
 ,
 
