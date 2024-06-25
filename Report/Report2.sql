7b3639e24e43c8e7943fae9cc00184e375173722
SMOKAL <Shreya.Padmakar.Mokal@gds.ey.com>
2024-06-26 03:09:33 +0530

    Update content between patterns

--- Report/Report2.sql
+++ Report/Report2.sql
@@ -18,13 +18,7 @@ Rule1Businessdata as
 /** Rule End :Rule1Businessdata     **/
 ,
 
-/** Rule Start :Rule5OrderCompletion     **/    
-Rule5OrderCompletion as
- (Select distinct o.orderid,o.orderstatus,u.uid
- from order o join
-      uname u
- on orderid=u.uid)
-/** Rule End :Rule5OrderCompletion     **/
+/** Rule Start :Rule5OrderCompletion     **/ Your new content goes here /** Rule End :Rule5OrderCompletion     **/
 
 
 ,
