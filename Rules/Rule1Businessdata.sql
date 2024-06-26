70c6372274bbbc54ae05b4bf62223e1a0ffdb43f
SMOKAL <Shreya.Padmakar.Mokal@gds.ey.com>
2024-06-26 20:33:56 +0530

    Update Rule1Businessdata.sql

--- Rules/Rule1Businessdata.sql
+++ Rules/Rule1Businessdata.sql
@@ -3,7 +3,7 @@ Rule1Businessdata as
  (Select distinct p.id,p.name,d.profession
  from profile p join
       data d
- on p.id=d.contactid)This area the new changes
+ on p.id=d.contactid)This area the new changes1234
 
 
 
