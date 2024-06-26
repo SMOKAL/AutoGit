eb10a8ebd15db2fa6a12a074d98e2ebac0e99743
SMOKAL <Shreya.Padmakar.Mokal@gds.ey.com>
2024-06-26 12:39:23 +0530

    Update content between patterns

--- Report/Report3.sql
+++ Report/Report3.sql
@@ -34,13 +34,13 @@ Sales_CTE AS (
 ),
 
 
-#RuleStart:Rule6ValidationProof#    
+#RuleStart:Rule6ValidationProof#   
 cte1 as
  (Select v.validationid,b.time from table1
   where v.status='Married'),
 Rule6ValidationProof as 
   (Select s.id,s.name,c.validationid from soap s join
-   cte1 c)
+   cte1 c)Changedoinginthecode
 #RuleEnd:Rule6ValidationProof#
 
 
