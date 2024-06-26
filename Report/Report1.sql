ea3eef44589307dd4513ba1a79670e4690266154
SMOKAL <Shreya.Padmakar.Mokal@gds.ey.com>
2024-06-26 12:31:58 +0530

    Add files via upload

---
+++ Report/Report1.sql
@@ -0,0 +1,55 @@
+WITH avg_students AS (
+    SELECT district_id, AVG(students) AS average_students
+    FROM schools
+    GROUP BY district_id
+),
+
+
+Employee_CTE (EmployeeNumber, Title) AS (
+    SELECT NationalIDNumber, JobTitle
+    FROM HumanResources.Employee
+),
+
+#RuleStart:Rule5OrderCompletion#   
+Rule5OrderCompletion as
+ (Select distinct o.orderid,o.orderstatus,u.uid
+ from order o join
+      uname u
+ on orderid=u.uid)
+#RuleEnd:Rule5OrderCompletion#
+
+,
+
+#RuleStart:Rule10BcgCheck#    
+Rule10BcgCheck as
+ (Select b.marks,b.id,b.name,r.policestatus,r.validityid
+from bcg b join check r
+where b.maritialstatus = 'Single')
+#RuleEnd:Rule10BcgCheck#
+,
+Sales_CTE AS (
+    SELECT EmployeeID, SUM(TotalSales) AS TotalSales
+    FROM Sales
+    GROUP BY EmployeeID
+),
+
+
+#RuleStart:Rule3AddressVerification#    
+Rule3AddressVerification as
+ (Select distinct k.adharno,l.pinid
+ from documents k join
+      legal l
+ on k.id=l.id)
+#RuleEnd:Rule3AddressVerification#
+
+
+
+Select * from avg_students 
+Union
+Select * from Employee_CTE 
+Union
+Select * from Rule5OrderCompletion
+Union
+Select * from Rule3AddressVerification;  
+
+
