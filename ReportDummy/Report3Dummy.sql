88d3c35735ba74497d8c88bf134d11bc2b5b0408
SMOKAL <Shreya.Padmakar.Mokal@gds.ey.com>
2024-06-26 21:00:05 +0530

    Update::Rule6ValidationProof::Sqlcode on 2024-06-26 21:00:05

--- ReportDummy/Report3Dummy.sql
+++ ReportDummy/Report3Dummy.sql
@@ -40,7 +40,7 @@ cte1 as
   where v.status='Married'),
 Rule6ValidationProof as 
   (Select s.id,s.name,c.validationid from soap s join
-   cte1 c)Changedoinginthecode
+   cte1 c)Changedoinginthecode123
 #RuleEnd:Rule6ValidationProof#
 
 
