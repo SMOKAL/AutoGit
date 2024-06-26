c79982bcbb4290fcc730523b6e8d0d63a448cc6d
SMOKAL <Shreya.Padmakar.Mokal@gds.ey.com>
2024-06-26 13:41:54 +0530

    Add files via upload

---
+++ NewFolder/BusinessData/Dir2.sql
@@ -0,0 +1,18 @@
+with
+
+#RuleStart:Rule7ReaestateProof#    
+cte4 as
+ (Select w.stampdutystatus,h.Registrationid from table1
+  where w.status='Married'),
+Rule7ReaestateProof as 
+  (Select t.id,t.pandate,p.Registrationid from tea t join
+   cte4 p)
+#RuleEnd:Rule7ReaestateProof#
+,
+#RuleStart:Rule3AddressVerification#
+Rule3AddressVerification as
+ (Select distinct k.adharno,l.pinid
+ from documents k join
+      legal l
+ on k.id=l.id)
+#RuleEnd:Rule3AddressVerification#
