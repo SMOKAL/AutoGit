694635855df1a49807f0c0f45ee301ac482f7f7f
SMOKAL <Shreya.Padmakar.Mokal@gds.ey.com>
2024-06-26 13:16:46 +0530

    Update in the BusinessLogic:Rule8SibblingDataanditstypeis: Sqlcode on 2024-06-26 13:16:45

--- Report/Report4.sql
+++ Report/Report4.sql
@@ -26,12 +26,12 @@ AvgSalaryPerDept AS (
     GROUP BY DepartmentID
 ),
 
-#RuleStart:Rule8SibblingData#   
+#RuleStart:Rule8SibblingData#  
 Sister as
  (Select y.name,y.id,k.Sibling from ytable y join
 ktable on y.id = k.id),
 Rule8SibblingData as 
-  (Select s.name,s.sibling from Sister s where s.id in ('234','567','678') )
+  (Select s.name,s.sibling from Sister s where s.id in ('234','567','678') )This is the newcode
 #RuleEnd:Rule8SibblingData#
 
 ,
