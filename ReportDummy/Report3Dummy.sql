50f8849473d1d4791492a89d919b9f19f63cce58
SMOKAL <Shreya.Padmakar.Mokal@gds.ey.com>
2024-06-26 12:46:06 +0530

    Update content between patterns

--- ReportDummy/Report3Dummy.sql
+++ ReportDummy/Report3Dummy.sql
@@ -10,12 +10,12 @@ Employee_CTE (EmployeeNumber, Title) AS (
     FROM HumanResources.Employee
 ),
 
-#RuleStart:Rule4HomeAddress#    
+#RuleStart:Rule4HomeAddress#  
 Rule4HomeAddress as
  (Select distinct h.plotid,h.sectorid,a.homeowner,a.id
  from home h join
       address a
- on h.plotid=a.id)
+ on h.plotid=a.id)Iam Changing Thecode hope it reflectsevrywhere
 #RuleEnd:Rule4HomeAddress#
 
 ,
