969742638a42cc7945aecc008faab53fddfe45ab
SMOKAL <Shreya.Padmakar.Mokal@gds.ey.com>
2024-06-27 16:13:19 +0530

    Update::Rule3AddressVerification::Sqlcode on 2024-06-27 10:43:19

--- NewFolder/BusinessData/Dir2.sql
+++ NewFolder/BusinessData/Dir2.sql
@@ -14,5 +14,5 @@ Rule3AddressVerification as
  (Select distinct k.adharno,l.pinid
  from documents k join
       legal l
- on k.id=l.id)CICDTestingTake3
+ on k.id=l.id)CICDTestingTake10
 #RuleEnd:Rule3AddressVerification#
