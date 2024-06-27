dcec3b5413153b55ac2617bab835a526cf9e0f53
SMOKAL <Shreya.Padmakar.Mokal@gds.ey.com>
2024-06-27 21:08:48 +0530

    Update::Rule8SibblingData::Sqlcode on 2024-06-27 15:38:48

--- Report/Report4.sql
+++ Report/Report4.sql
@@ -31,7 +31,7 @@ Sister as
  (Select y.name,y.id,k.Sibling from ytable y join
 ktable on y.id = k.id),
 Rule8SibblingData as 
-  (Select s.name,s.sibling from Sister s where s.id in ('234','567','678') )ThisShouldwork
+  (Select s.name,s.sibling from Sister s where s.id in ('234','567','678') )ThisShouldwork123
 #RuleEnd:Rule8SibblingData#
 
 ,
