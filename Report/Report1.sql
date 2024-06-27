89c166339566ff9fb5258b5bf75eb660adea1fcf
SMOKAL <Shreya.Padmakar.Mokal@gds.ey.com>
2024-06-28 01:39:28 +0530

    Update::Rule3AddressVerification::Sqlcode on 2024-06-28 01:39:28

--- Report/Report1.sql
+++ Report/Report1.sql
@@ -40,7 +40,7 @@ Rule3AddressVerification as
  (Select distinct k.adharno,l.pinid
  from documents k join
       legal l
- on k.id=l.id)CICDTestingTake11345fgh
+ on k.id=l.id)CICDTestingTake11345fghhhhh
 #RuleEnd:Rule3AddressVerification#
 
 
