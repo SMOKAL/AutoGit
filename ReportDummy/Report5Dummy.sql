65404bf87715e784d2203926953feb3becb1f892
SMOKAL <Shreya.Padmakar.Mokal@gds.ey.com>
2024-06-27 16:24:18 +0530

    Update::Rule10BcgCheck::Sqlcode on 2024-06-27 10:54:18

--- ReportDummy/Report5Dummy.sql
+++ ReportDummy/Report5Dummy.sql
@@ -39,7 +39,7 @@ ftable on e.id = f.id)This is the CICDTestingShreya
 Rule10BcgCheck as
  (Select b.marks,b.id,b.name,r.policestatus,r.validityid
 from bcg b join check r
-where b.maritialstatus = 'Single')CICDTAKE10
+where b.maritialstatus = 'Single')CICD-SHREYA
 
 #RuleEnd:Rule10BcgCheck#
 
