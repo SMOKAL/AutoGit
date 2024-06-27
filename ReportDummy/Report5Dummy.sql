6c3f1efb7b2b4636cc8371bba29250d7ccbe1755
SMOKAL <Shreya.Padmakar.Mokal@gds.ey.com>
2024-06-28 00:35:19 +0530

    Update::Rule9ExtraExpense::Sqlcode on 2024-06-28 00:35:19

--- ReportDummy/Report5Dummy.sql
+++ ReportDummy/Report5Dummy.sql
@@ -29,7 +29,7 @@ AvgSalaryPerDept AS (
 #RuleStart:Rule9ExtraExpense#    
 Rule9ExtraExpense as
  (Select e.Expense,e.id,f.name from expensetable e join
-ftable on e.id = f.id)This is the CICDTestingShreya123
+ftable on e.id = f.id)This is the CICDTestingShreya123abcdef
 
 #RuleEnd:Rule9ExtraExpense#
 
