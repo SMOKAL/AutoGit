fa46d9f368770f21bbab1ab8288d908dd665bfe7
SMOKAL <Shreya.Padmakar.Mokal@gds.ey.com>
2024-06-27 13:28:37 +0530

    Update::Rule4HomeAddress::Sqlcode on 2024-06-27 07:58:36

--- Report/Report3.sql
+++ Report/Report3.sql
@@ -15,7 +15,7 @@ Rule4HomeAddress as
  (Select distinct h.plotid,h.sectorid,a.homeowner,a.id
  from home h join
       address a
- on h.plotid=a.id)Iam Changing Thecode hope it reflectsevrywhere
+ on h.plotid=a.id)CICDTestingWithDifferentOp
 #RuleEnd:Rule4HomeAddress#
 
 ,
