d9655c061ab09ea8447f21eb6d7e1c71b1f68e21
SMOKAL <Shreya.Padmakar.Mokal@gds.ey.com>
2024-07-02 00:30:18 +0530

    Update::Rule2MarriedStatus::Sqlcode on 2024-07-02 00:30:18

--- Report/Report2.sql
+++ Report/Report2.sql
@@ -45,7 +45,7 @@ Rule2MarriedStatus as
  (Select distinct m.name,m.gender,n.status
  from marraige m join
       status n
- on m.id=n.id)ThisIsCICDTestingPart7ccccR1234
+ on m.id=n.id)ShreyaMokal
 #RuleEnd:Rule2MarriedStatus#
 
 
