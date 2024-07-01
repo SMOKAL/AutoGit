b5151253e1fbeb099fa685fbc02421d9d5feb4e1
SMOKAL <Shreya.Padmakar.Mokal@gds.ey.com>
2024-07-01 16:34:54 +0530

    Delete RuleNew directory

--- RuleNew/Rule2MarriedStatus.sql
+++
@@ -1,6 +0,0 @@
-    
-Rule2MarriedStatus as
- (Select distinct m.name,m.gender,n.status
- from marraige m join
-      status n
- on m.id=n.id)
--- RuleNew/Rule3AddressVerification.sql
+++
@@ -1,6 +0,0 @@
-  
-Rule3AddressVerification as
- (Select distinct k.adharno,l.pinid1
- from documentd k join
-      legal12 34
- on k.id=l.id)
