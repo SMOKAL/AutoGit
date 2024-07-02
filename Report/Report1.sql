96914f765640c3a71e1d9432005ea44bc552a5a1
SMOKAL <Shreya.Padmakar.Mokal@gds.ey.com>
2024-07-03 01:25:25 +0530

    Update::Rule10BcgCheck::Sqlcode on 2024-07-03 01:25:25

--- Report/Report1.sql
+++ Report/Report1.sql
@@ -23,7 +23,8 @@ Employee_CTE (EmployeeNumber, Title) AS (
 Rule10BcgCheck as
  (Select b.marks,b.id,b.name,r.policestatus,r.validityid
 from bcg b join check r
-where b.maritialstatus = 'Single')12
+where b.maritialstatus = 'Single')
+
 
 #RuleEnd:Rule10BcgCheck#
 ,
