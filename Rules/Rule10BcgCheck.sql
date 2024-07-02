2aefc4110b9b4f9525e2d6ee29ceb261bb7ed55a
SMOKAL <Shreya.Padmakar.Mokal@gds.ey.com>
2024-07-02 12:39:03 +0530

    Update::Rule10BcgCheck.sql

--- Rules/Rule10BcgCheck.sql
+++ Rules/Rule10BcgCheck.sql
@@ -2,5 +2,5 @@
 Rule10BcgCheck as
  (Select b.marks,b.id,b.name,r.policestatus,r.validityid
 from bcg b join check r
-where b.maritialstatus = 'Single')CICD-SHREYAMokal23shreya
+where b.maritialstatus = 'Single')CICD-SHREYAMokal2
 
