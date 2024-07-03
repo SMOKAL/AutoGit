61ab097cf86786c6e694667ab317776ab64edc2e
SMOKAL <Shreya.Padmakar.Mokal@gds.ey.com>
2024-07-03 19:24:32 +0530

    Update::Rule7ReaestateProof::Sqlcode on 2024-07-03 19:24:31

--- Report/Report4.sql
+++ Report/Report4.sql
@@ -15,9 +15,9 @@ cte4 as
   where w.status='Married'),
 Rule7ReaestateProof as 
   (Select t.id,t.pandate,p.Registrationid from tea t join
-   cte4 p)123ch12
+   cte4 p) 
 
- #RuleEnd:Rule7ReaestateProof#
+  #RuleEnd:Rule7ReaestateProof#
 
 
 ,
