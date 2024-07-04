f67833dfcb7ad2baefed4983c7ee33eecf7a8c19
SMOKAL <Shreya.Padmakar.Mokal@gds.ey.com>
2024-07-04 12:22:41 +0530

    Update::Rule5OrderCompletion::Sqlcode on 2024-07-04 12:22:41

--- Report/Report1.sql
+++ Report/Report1.sql
@@ -15,8 +15,8 @@ Rule5OrderCompletion as
  (Select distinct o.orderid,o.orderstatus,u.uid
  from order o join
       uname u
- on orderid=u.uid)123456789
- #RuleEnd:Rule5OrderCompletion#
+ on orderid=u.uid)
+  #RuleEnd:Rule5OrderCompletion#
 
 ,
 
