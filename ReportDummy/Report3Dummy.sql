09553853cbb0f56a980daa201b6f6b221f8f7b53
SMOKAL <Shreya.Padmakar.Mokal@gds.ey.com>
2024-07-04 14:40:03 +0530

    Update::Rule4HomeAddress::Sqlcode on 2024-07-04 14:40:03

--- ReportDummy/Report3Dummy.sql
+++ ReportDummy/Report3Dummy.sql
@@ -15,9 +15,10 @@ Rule4HomeAddress as
  (Select distinct h.plotid,h.sectorid,a.homeowner,a.id
  from home h join
       address a
- on h.plotid=a.id)124
+ on h.plotid=a.id)124Change3
 
-     #RuleEnd:Rule4HomeAddress#
+      
+#RuleEnd:Rule4HomeAddress#
 
 ,
 #RuleStart:Rule1Businessdata#
