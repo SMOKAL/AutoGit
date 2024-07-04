223496c5a72a3bd025e2f42d99e698b23c9fa170
SMOKAL <Shreya.Padmakar.Mokal@gds.ey.com>
2024-07-04 17:35:09 +0530

    Update::Rule7ReaestateProof::Sqlcode on 2024-07-04 17:35:09

--- NewFolder/BusinessData/Dir2.sql
+++ NewFolder/BusinessData/Dir2.sql
@@ -6,7 +6,9 @@ cte4 as
   where w.status='Married'),
 Rule7ReaestateProof as 
   (Select t.id,t.pandate,p.Registrationid from tea t join
-   cte4 p)
+   cte4 p)123fghyui 
+
+   
 #RuleEnd:Rule7ReaestateProof#
 ,
 #RuleStart:Rule3AddressVerification#  
