dbcc8a0803b26d63a9c4271fec794eb09540ecd3
SMOKAL <Shreya.Padmakar.Mokal@gds.ey.com>
2024-07-09 21:58:37 +0530

    Update Rule10BcgCheck.sql

--- Rules/Rule10BcgCheck.sql
+++ Rules/Rule10BcgCheck.sql
@@ -2,7 +2,7 @@
 Rule10BcgCheck as
  (Select b.marks,b.id,b.name,r.policestatus,r.validityid
 from bcg b join check r
-where b.maritialstatus = 'Single')123
+where b.maritialstatus = 'Single')1235679
 
 
 
