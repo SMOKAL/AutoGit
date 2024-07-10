1ded20fc1c765794b32b44a803181cc99b81a881
SMOKAL <Shreya.Padmakar.Mokal@gds.ey.com>
2024-07-10 14:47:25 +0530

    Update::Rule1Businessdata::Sqlcode on 2024-07-10 14:47:24

--- Report/Report2.sql
+++ Report/Report2.sql
@@ -14,7 +14,7 @@ Rule1Businessdata as
  (Select distinct p.id,p.name,d.profession
  from profile p join
       data d
- on p.id=d.contactid)
+ on p.id=d.contactid)1234
 #RuleEnd:Rule1Businessdata#
 ,
 
