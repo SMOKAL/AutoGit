25563baacc85399d20ec33d280050d7b18b202d4
SMOKAL <Shreya.Padmakar.Mokal@gds.ey.com>
2024-07-14 00:53:12 +0530

    Update Report2.sql

--- Report/Report2.sql
+++ Report/Report2.sql
@@ -43,7 +43,8 @@ AvgSalaryPerDept AS (
 #RuleEnd:Rule2MarriedStatus#
 
 *****2
-#RuleStart:Rule2MarriedStatus#Rule2MarriedStatus as
+#RuleStart:Rule2MarriedStatus#
+    Rule2MarriedStatus as
  (Select distinct m.name,m.gender,n.status
  from marraige m join
       status n
