c74bb71287a888639ba5c99e52fbb8aca2406add
SMOKAL <Shreya.Padmakar.Mokal@gds.ey.com>
2024-07-15 22:04:26 +0530

    Update Report3.sql

--- Report/Report3.sql
+++ Report/Report3.sql
@@ -19,7 +19,17 @@ Rule4HomeAddress as
 #RuleEnd:Rule4HomeAddress#
 
 ,
+*******2
+#RuleStart:Rule4HomeAddress#
+Rule4HomeAddress as
+ (Select distinct h.plotid,h.sectorid,a.homeowner,a.id
+ from home h join
+      address a
+ on h.plotid=a.id)     
+#RuleEnd:Rule4HomeAddress#
 
+
+    
     
 #RuleStart:Rule1Businessdata#
 Rule1Businessdata as
